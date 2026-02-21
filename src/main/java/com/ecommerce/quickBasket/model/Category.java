9219b122257d1348e6002d5a201defd5f5d7ca59
palakpant <pantpalak2000@gmail.com>
2026-02-21 22:39:21 +0530

    added Generation type

--- src/main/java/com/ecommerce/quickBasket/model/Category.java
+++ src/main/java/com/ecommerce/quickBasket/model/Category.java
@@ -1,12 +1,15 @@
 package com.ecommerce.quickBasket.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name="categories")
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long categoryId;
     private String categoryName;
 
